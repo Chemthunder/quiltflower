26e6bdb3b7d4bc67f9c2ad74c8b573bd47d29686
Chemm :3 <chemthunderisme@gmail.com>
2026-03-04 12:12:43 -0500

    started on an emulater menu

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 console.log("Thank you for using Quiltflower game emulator! Consider supporting me on GitHub: " + "github.com/chemthunder" + "! Thank you!");
 console.log("If you enjoyed using Quiltflower, check out my other projects, such as Hyacinth!");
-let showcaseEnabled = true;
+let showcaseEnabled = false;
+let isGameLoaded = false;
+
 
 namespace Quiltflower {
     if (showcaseEnabled) {
-
         let testCart: Emulator.Cart = Emulator.bootstrapNew("test", 0);
         let secondCart: Emulator.Cart = Emulator.bootstrapNew("secondTest", 1);
         // registers two carts, using a string id and a number id.
@@ -12,33 +13,36 @@ namespace Quiltflower {
         Emulator.loadCart(testCart, _load);
         // loads a cart, and connects a load function that will run upon start.
 
-
         // the ran function upon loading a cart
         function _load() {
-            QuiltCarts.run(Emulator.getLoadedCart().id); // uses a dedicated namespace for the carts
+            run(Emulator.getLoadedCart().id);
         }
-    }
-}
 
-namespace QuiltCarts {
-    export function run(id: number): void {
-        switch (id) {
-            case (0): {
-                run0();
-                break;
-            }
-            case (1): {
-                run1();
-                break;
+        function run(id: number): void {
+            switch (id) {
+                case (0): {
+                    run0();
+                    break;
+                }
+                case (1): {
+                    run1();
+                    break;
+                }
             }
         }
+
+        function run0() { // this will run if the loaded cart's id is 0
+            console.log("a");
+        }
+
+        function run1() {
+            console.log("b");
+        }
     }
 
-    function run0() { // this will run if the loaded cart's id is 0
-        console.log("a");
-    }
-
-    function run1() {
-        console.log("b");
-    }
+    let hoveredCart:Emulator.Cart;
+    
+    controller.A.onEvent(ControllerButtonEvent.Pressed, function() {
+        
+    });
 }
