bb086166e30a726a44a894515df2f388c82e0477
Chemm :3 <chemthunderisme@gmail.com>
2026-03-04 13:22:36 -0500

    finished selection menu

--- main.ts
+++ main.ts
@@ -5,12 +5,17 @@ let isGameLoaded = false;
 
 
 namespace Quiltflower {
+    let hover: number;
+
     let testCart: Emulator.Cart = Emulator.bootstrapNew("Test1", 0);
     let secondTest: Emulator.Cart = Emulator.bootstrapNew("Test2", 1);
-    let c = 0;
+    let thirdTest: Emulator.Cart = Emulator.bootstrapNew("Test3", 2);
+
+    let c = -4;
+    hover = 1;
 
     for (let cart of Emulator.cartStrings) {
-        c ++;
+        c++;
         let cartReadout = sprites.create(img`
             . . . . . . . . . . . . . . . .
             . . . . . . . . . . . . . . . .
@@ -30,17 +35,27 @@ namespace Quiltflower {
             . . . . . . . . . . . . . . . .
         `);
 
-        cartReadout.setPosition(scene.screenWidth() / 2, 0);
-        cartReadout.setPosition(cartReadout.x, 60 + c * 8);
+        cartReadout.setPosition(20 + cart.length, 100);
+        cartReadout.setPosition(cartReadout.x, 60 + c * 12);
 
         cartReadout.sayText(cart, Infinity, false, 1, 15);
     }
 
-    Emulator.loadCart(testCart, _draw);
+    controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+        if (!isGameLoaded) {
+            if (Emulator.carts.length > 0) {
+                if (hover != Emulator.carts.length) {
+                    hover++;
+                } else {
+                    hover = 1;
+                }
+            }
+        }
+    });
 
-    function _draw() {
-
-    }
-
-    game.showLongText(Emulator.carts.length, DialogLayout.Bottom);
+    forever(function () {
+        if (!isGameLoaded) {
+            info.setScore(hover);
+        }
+    });
 }
