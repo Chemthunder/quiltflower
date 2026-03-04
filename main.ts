a4016a70b5040a28ccf58a7a2f826370a8331542
Chemm :3 <chemthunderisme@gmail.com>
2026-03-04 11:31:20 -0500

    Renamed a function

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 namespace Quiltflower {
     export let testCart:Emulator.Cart = Emulator.bootstrapNew("test", 0);
     export let secondCart:Emulator.Cart = Emulator.bootstrapNew("secondTest", 1);
-    Emulator.loadCart(secondCart, loadEvent);
+    Emulator.loadCart(secondCart, _load);
 
 
 
-    export function loadEvent() {
+    export function _load() {
         if (Emulator.getLoadedCart() == testCart) {
             console.log("a");
         } else {
