3c3db94d0e8674c1a037f9f9813d9e68fd30ad13
Chemm :3 <chemthunderisme@gmail.com>
2026-03-04 11:32:24 -0500

    added comments for clarity

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 namespace Quiltflower {
     export let testCart:Emulator.Cart = Emulator.bootstrapNew("test", 0);
     export let secondCart:Emulator.Cart = Emulator.bootstrapNew("secondTest", 1);
+    // registers two carts, using a string id and a number id.
+
     Emulator.loadCart(secondCart, _load);
+    // loads a cart, and connects a load function that will run upon start.
 
 
-
+    // the ran function upon loading a cart
     export function _load() {
         if (Emulator.getLoadedCart() == testCart) {
             console.log("a");
